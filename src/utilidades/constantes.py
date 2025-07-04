a7c07f0e9602e1ef75d9725ace66d31d3d4575d7
Juan Pablo Zappettini <juanzappettini6@gmail.com>
2025-07-04 12:14:43 -0300

    Ventas limpio

--- src/utilidades/constantes.py
+++ src/utilidades/constantes.py
@@ -6,3 +6,4 @@ DATA_PATH = Path("../data")  # Carpeta de entrada con archivos Excel
 DATA_PROCESADA_PATH = Path("../data_procesada")  # Carpeta de salida para archivos procesados
 PATRON_ARCHIVOS_PATH = "ventas_detalle_*.xlsx"  # Patrón para buscar archivos Excel
 ARCHIVO_SALIDA_PATH = DATA_PROCESADA_PATH / "ventas_historico.csv"  # Archivo CSV de salida unificado
+DATA_LIMPIA_PATH = DATA_PROCESADA_PATH / "ventas_limpio.csv"  # Archivo CSV limpio de salida
