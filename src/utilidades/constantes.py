6ad96cd03337d3698d05ea4f0e2ae5a922a65782
Juan Pablo Zappettini <juanzappettini6@gmail.com>
2025-07-07 11:50:12 -0300

    Agrego inlfacion

--- src/utilidades/constantes.py
+++ src/utilidades/constantes.py
@@ -7,3 +7,4 @@ DATA_PROCESADA_PATH = Path("../data_procesada")  # Carpeta de salida para archiv
 PATRON_ARCHIVOS_PATH = "ventas_detalle_*.xlsx"  # Patrón para buscar archivos Excel
 ARCHIVO_SALIDA_PATH = DATA_PROCESADA_PATH / "ventas_historico.csv"  # Archivo CSV de salida unificado
 DATA_LIMPIA_PATH = DATA_PROCESADA_PATH / "ventas_limpio.csv"  # Archivo CSV limpio de salida
+DATA_SIN_INFLACION = DATA_PROCESADA_PATH / "ventas_ajustado_a_jun25.csv"  # Archivo CSV ajustado por inflación
