e8e7560a4b24e162560f8f42527845456bc9df4f
Juan Pablo Zappettini <juanzappettini6@gmail.com>
2025-07-07 15:25:38 -0300

    Facturas

--- src/utilidades/constantes.py
+++ src/utilidades/constantes.py
@@ -8,3 +8,5 @@ PATRON_ARCHIVOS_PATH = "ventas_detalle_*.xlsx"  # Patrón para buscar archivos E
 ARCHIVO_SALIDA_PATH = DATA_PROCESADA_PATH / "ventas_historico.csv"  # Archivo CSV de salida unificado
 DATA_LIMPIA_PATH = DATA_PROCESADA_PATH / "ventas_limpio.csv"  # Archivo CSV limpio de salida
 DATA_SIN_INFLACION = DATA_PROCESADA_PATH / "ventas_ajustado_a_jun25.csv"  # Archivo CSV ajustado por inflación
+DATA_FACTURAS_PATH = DATA_PATH / "VENTAS_JUNIO_2025.txt"  # Archivo txt de facturas procesadas
+DATA_FINAL_FACTURAS_PATH = DATA_PROCESADA_PATH / "ventas_final.txt"  # Archivo txt final de facturas procesadas
