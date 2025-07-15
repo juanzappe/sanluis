03e448667279f4405f4cc0ba3e1da957b9019580
Juan Pablo Zappettini <juanzappettini6@gmail.com>
2025-07-15 16:40:30 -0300

    MEJORA

--- src/utilidades/constantes.py
+++ src/utilidades/constantes.py
@@ -12,3 +12,5 @@ MOSTRADOR_SALIDA_PATH = DATA_PROCESADA_PATH / "ventas_mostrador.csv"  # Archivo 
 # FACTURACION
 PATRON_FACTURACION_PATH = "FACTURACION_*.txt"  # Archivo txt de facturas procesadas
 FACTURACION_SALIDA_PATH = DATA_PROCESADA_PATH / "ventas_facturacion.csv"  # Archivo txt final de facturas procesadas
+
+ARCHIVO_UNIFICADO_PATH = DATA_PROCESADA_PATH / "ventas_unificadas.csv"  # Archivo CSV final unificado
